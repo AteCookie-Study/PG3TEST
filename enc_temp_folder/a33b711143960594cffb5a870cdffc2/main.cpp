4ba56d6957101dd46d30b796ed26530225a2a6bf
AteCookie <k024g1012@g.neec.ac.jp>
2025-04-18 14:29:10 +0900

    01_01

--- enc_temp_folder/a33b711143960594cffb5a870cdffc2/main.cpp
+++ enc_temp_folder/a33b711143960594cffb5a870cdffc2/main.cpp
@@ -2,6 +2,7 @@
 #define NOMINMAX
 #include <Windows.h>
 
+// 模板函數：取較小值
 template <typename T>
 T min(T a, T b) {
     return (a < b) ? a : b;
@@ -11,10 +12,10 @@ int main() {
     int a = 10, b = 20;
     float d = 5.5f, e = 3.3f;
 
-    printf("より小さい整数: %d\n", min(a, b));
-    printf("より小さい浮動小数点数: %.2f\n", min(d, e));
+    printf("较小的整数: %d\n", min(a, b));
+    printf("较小的浮点数: %.2f\n", min(d, e));
 
-    system("pause");  
+    system("pause");
 
     return 0;
 }
